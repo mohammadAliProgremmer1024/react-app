396bb04bf460c01552d02243b630f857118a0106
mohammadAli <mohammadalipython1024@gmail.com>
2025-02-12 21:30:01 -0800

    react project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+// import StarRaiting from './starRaiting';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot( document.getElementById('root') );
+root.render(
+    <App />
+    // <>
+        // <StarRaiting />
+        // <StarRaiting maxRaiting={10} />
+    // </>
+);
